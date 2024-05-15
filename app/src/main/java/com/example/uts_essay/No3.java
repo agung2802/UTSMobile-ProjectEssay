647c22accc9f3c44484fc4c26837a57d14230117
Faisal Mulya Santosa - 3RT8D <3rt8d@mail.com>
2024-05-15 19:01:22 +0700

    first commit

---
+++ app/src/main/java/com/example/uts_essay/No3.java
@@ -0,0 +1,6 @@
+package com.example.uts_essay;
+
+public class No3 {
+    private String message;
+
+}
---
+++ app/src/main/java/com/example/uts_essay/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.example.uts_essay
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+    }
+}
